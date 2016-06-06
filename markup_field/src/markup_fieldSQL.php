3c5911878a25e089b49b8a842692a661e34545ec
ferdly <brad@qiqgroup.com>
2016-06-06 12:33:38 -0500

    markup has no data, just instantiate and mark join & select as hidden

---
+++ markup_field/src/markup_fieldSQL.php
@@ -0,0 +1,22 @@
+<?php
+
+class markup_fieldSQL extends fieldSQL {
+
+    public function __construct($field_array) {
+        parent::__construct($field_array);
+        $this->field_join_is_hidden = 1;
+        $this->field_select_is_hidden = 1;
+    }
+
+
+    /**
+     * Most Current OO from Local Static Method
+     *
+     *
+     */
+
+
+    /**
+     * END Most Current OO from Local Static Method
+     */
+} //END class markup_fieldSQL extends fieldSQL
