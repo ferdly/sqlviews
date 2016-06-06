fda944735bc6a5157c96410beea0151da9b41e21
ferdly <brad@qiqgroup.com>
2016-06-06 12:34:59 -0500

    cursory expectation is that is essentially a text field with email gizmology

---
+++ email_field/src/email_fieldSQL.php
@@ -0,0 +1,20 @@
+<?php
+
+class email_fieldSQL extends fieldSQL {
+
+    public function __construct($field_array) {
+        parent::__construct($field_array);
+    }
+
+
+    /**
+     * Most Current OO from Local Static Method
+     *
+     *
+     */
+
+
+    /**
+     * END Most Current OO from Local Static Method
+     */
+} //END class markup_fieldSQL extends fieldSQL
